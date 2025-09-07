06975916557d912459e4730dfd4816ac2d532368
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-09-07 17:00:15 +0300

    feat: Fixed the trajet api functions

--- api/trajet.ts
+++ api/trajet.ts
@@ -1,8 +1,28 @@
 import { BASE_API_URL } from "@/env";
-import axiosAuthInstance from "./Config";
+import axiosAuthInstance, { axiosInstance } from "./Config";
 
 const TRAJET_API_URL = `${BASE_API_URL}/trajet`;
 
 export const getAllTrajet = async () => {
-    return await axiosAuthInstance.get(`${TRAJET_API_URL}/trajet`);
+    return await axiosAuthInstance.get(`${TRAJET_API_URL}/all`);
+}
+
+export const getTrajetById = async (id: any) => {
+    return await axiosAuthInstance.get(`${TRAJET_API_URL}/get/${id}`);
+}
+
+export const searchTrajet = async (data: any) => {
+    return await axiosInstance.post(`${TRAJET_API_URL}/search`, data);
+}
+
+export const postTrajet = async (data: any) => {
+    return await axiosAuthInstance.post(`${TRAJET_API_URL}/create`, data);
+}
+
+export const deleteTrajet = async (id: any) => {
+    return await axiosAuthInstance.delete(`${TRAJET_API_URL}/delete/${id}`);
+}
+
+export const editTrajet = async (id: any, data: any) => {
+    return await axiosAuthInstance.patch(`${TRAJET_API_URL}/edit/${id}, data`);
 }
