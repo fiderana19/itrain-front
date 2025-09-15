53e21db2582e8d7f6cfdbe5cd3045f560f83acbd
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-09-15 18:07:23 +0300

    Fixed the admin reservation page

--- app/(admin)/adminhome.tsx
+++ app/(admin)/adminhome.tsx
@@ -1,33 +1,8 @@
 import { colorBlue } from '@/constants/Colors';
 import { stylesPerso } from '@/src/styles/GeneralStyles';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { router } from 'expo-router';
-import { useEffect } from 'react';
 import { StyleSheet, View , Text , ScrollView } from 'react-native';
-import { useAuth } from '../../context/AuthContext';
-import Toast from 'react-native-toast-message';
 
 export default function HomeScreen() {
-  const { isAuthenticated } = useAuth();
-
-  useEffect(() => {
-    console.log("ett0", isAuthenticated)
-    verifyToken()
-
-    Toast.show({
-      type: 'success',
-      text1: 'Hello',
-      text2: 'This is some something 👋'
-    });
-  }, [])
-  const verifyToken = async () => {
-    const token = await AsyncStorage.getItem('token')
-
-    if(token === '') {
-      router.replace('/home')           
-    }
-  }
-
   return (
       <ScrollView style={stylesPerso.container}>
         <View>
